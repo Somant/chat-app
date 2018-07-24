2fa72a0e66e033027e29d78e8ab4040632487815
Somant <somant@gmail.com>
2018-07-24 19:05:18 +0530

    Add Geolocation Support

--- public/js/index.js
+++ public/js/index.js
@@ -15,13 +15,16 @@ var socket= io();
                 jQuery('#messages').append(li);
                 
           });
-
-          socket.emit('createMessage',{
-            from: 'frank',
-            text:'Hi'  
-          },function(data){
-              console.log('Got it:', data);
+          socket.on('newLocationMessage',function(message){
+            var li= jQuery('<li></li>');
+            var a= jQuery('<a target="_blank">My current Location</a>')
+            li.text(`${message.from}: `);
+            a.attr('href',message.url)
+            li.append(a);
+            jQuery('#messages').append(li);
           });
+
+         
 
           jQuery('#message-form').on('submit',function(e){
              e.preventDefault();
@@ -31,4 +34,20 @@ var socket= io();
              },function(){
 
              });
+          });
+
+          var locationButton = jQuery('#send-location');
+          locationButton.on('click',function(){
+              if(!navigator.geolocation){
+                  return alert('Geolocation not supported by your browser');
+              }
+
+              navigator.geolocation.getCurrentPosition(function(position){
+                socket.emit('createLocationMessage',{
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },function(){
+                  alert('Unable to fetch location.')
+              });
           });
